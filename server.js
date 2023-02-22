2c161fb277fea7b234626fb532a6e9bda0c6b61c
Don Davidov <shok16e@gmail.com>
2023-02-21 22:49:03 -0700

    addedrts fe

--- server.js
+++ server.js
@@ -8,15 +8,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 
+/*const { MongoClient } = require('mongodb');
+
+async function main() {
+  const uri = 'mongodb+srv://vaha1455:Newdbpass9955@<cluster>.mongodb.net/social-network?retryWrites=true&w=majority';
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    console.log('Connected successfully to server');
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);*/
+
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/social-network",
   {
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
 mongoose.set("debug", true);
+
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
